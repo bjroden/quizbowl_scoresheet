e9f27cda15aed0d65d7e65df256f21af678d03b8
naedens <98190885+naedens@users.noreply.github.com>
2022-11-23 17:11:23 -0600

    added New Game button (but does not launch next intent yet)

--- app/src/main/java/com/example/quizbowlscoresheet/MainActivity.kt
+++ app/src/main/java/com/example/quizbowlscoresheet/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.quizbowlscoresheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var newGameBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        newGameBtn = findViewById(R.id.new_game_btn)
+        newGameBtn.setOnClickListener{
+            Log.d("MainActivity", "new game button was clicked")
+        }
     }
 }

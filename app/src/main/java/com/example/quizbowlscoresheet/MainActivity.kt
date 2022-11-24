7038f9189ed53857bb5ecde67cef7f561a6061c6
naedens <98190885+naedens@users.noreply.github.com>
2022-11-23 18:01:04 -0600

    added GameOverviewActivity and made MainActivity launch intent to it

--- app/src/main/java/com/example/quizbowlscoresheet/MainActivity.kt
+++ app/src/main/java/com/example/quizbowlscoresheet/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.quizbowlscoresheet
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var newGameBtn: Button
+
+    val startGameOverviewActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+        if(result.resultCode== Activity.RESULT_OK){
+            Log.d("MainActivity","Completed")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         newGameBtn = findViewById(R.id.new_game_btn)
         newGameBtn.setOnClickListener{
             Log.d("MainActivity", "new game button was clicked")
+            startGameOverviewActivity.launch(Intent(this,GameOverviewActivity::class.java))
         }
     }
 }

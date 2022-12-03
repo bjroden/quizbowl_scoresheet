ffc6da025a18395bdcd3128a47d96eddb4864d70
bjroden <brianjaredroden@gmail.com>
2022-12-02 19:19:33 -0600

    Add round 1, 2, and 4 properties and naive scores. Score implementation might be placeholder since it involves a lot of redundant calculations. Will take another stab at redoing the database schema so we can keep track of round scores on the DB side for better performance.

--- app/src/main/java/com/example/quizbowlscoresheet/database/models/GameAGQBA.kt
+++ app/src/main/java/com/example/quizbowlscoresheet/database/models/GameAGQBA.kt
@@ -38,4 +38,30 @@ data class GameAGQBA(
         entityColumn = "id"
     )
     val team2: Team?
-)
+) {
+    val round1Tossups by lazy { tossups.filter { it.roundNumber == 1 } }
+    val round2Tossups by lazy { tossups.filter { it.roundNumber == 2 } }
+    val round4Tossups by lazy { tossups.filter { it.roundNumber == 4 } }
+
+    val team1LightningRound by lazy { lightningCategories.find { it.categoryInfo.team == TeamAnswered.TEAM1 } }
+    val team2LightningRound by lazy { lightningCategories.find { it.categoryInfo.team == TeamAnswered.TEAM2 } }
+
+    val team1Round1Score by lazy { round1Score(TeamAnswered.TEAM1) }
+    val team1Round2Score by lazy { round2Score(TeamAnswered.TEAM1) }
+    val team1Round4Score by lazy { round4Score(TeamAnswered.TEAM1) }
+
+    val team2Round1Score by lazy { round1Score(TeamAnswered.TEAM2) }
+    val team2Round2Score by lazy { round2Score(TeamAnswered.TEAM2) }
+    val team2Round4Score by lazy { round4Score(TeamAnswered.TEAM2) }
+
+    fun roundNTossups(n: Int) = tossups.filter { it.roundNumber == n }
+
+    fun round1Score(team: TeamAnswered) = 10 * round1Tossups.count { it.team == team }
+    fun round2Score(team: TeamAnswered): Int {
+        val tossupsScore = 10 * round2Tossups.count { it.team == team }
+        val bonusScore = 5 * bonusCategories.filter { it.categoryInfo.team == team }
+            .sumOf { it.bonusQuestions.count { q -> q.answered } }
+        return tossupsScore + bonusScore
+    }
+    fun round4Score(team: TeamAnswered) = 10 * round4Tossups.count { it.team == team }
+}

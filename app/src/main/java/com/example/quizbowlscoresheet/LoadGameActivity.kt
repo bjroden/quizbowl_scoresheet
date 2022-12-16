f30dc6abe46f8c55f52c7cff2ff93395e8430ac3
bjroden <bjroden@uark.edu>
2022-12-16 15:07:44 -0600

    Add delete button to view

--- app/src/main/java/com/example/quizbowlscoresheet/LoadGameActivity.kt
+++ app/src/main/java/com/example/quizbowlscoresheet/LoadGameActivity.kt
@@ -1,6 +1,7 @@
 package com.example.quizbowlscoresheet
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,14 +12,22 @@ import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quizbowlscoresheet.database.models.Game
+import com.google.android.material.appbar.MaterialToolbar
 
 class LoadGameActivity : AppCompatActivity() {
+    val viewModel: LoadGameViewModel by viewModels {
+        LoadGameViewModel.LoadGameViewModelFactory((application as QuizbowlApplication).repository)
+    }
+
     val startGameOverviewActivity =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
                 Log.d("MainActivity", "Completed")
             }
         }
+
+    private var selectionMode = SelectionMode.LAUNCH
+    private lateinit var toolbar: MaterialToolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,18 +37,58 @@ class LoadGameActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = GridLayoutManager(this, 1, RecyclerView.VERTICAL, false)
 
-        val viewModel: LoadGameViewModel by viewModels {
-            LoadGameViewModel.LoadGameViewModelFactory((application as QuizbowlApplication).repository)
+        supportActionBar?.hide()
+        toolbar = findViewById(R.id.loadGameToolbar)
+        toolbar.inflateMenu(R.menu.load_activity_menu)
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.delete_game -> {
+                    setMode(SelectionMode.DELETE)
+                    true
+                }
+                else -> super.onOptionsItemSelected(it)
+            }
         }
+        setMode(SelectionMode.LAUNCH)
+
         viewModel.gameList.observe(this) { games ->
             adapter.submitList(games)
         }
     }
 
     private fun gameSelected(game: Game){
+        when (selectionMode) {
+            SelectionMode.LAUNCH -> launchGame(game)
+            SelectionMode.DELETE -> deleteGame(game)
+        }
+        setMode(SelectionMode.LAUNCH)
+    }
+
+    private fun launchGame(game: Game) {
         val intent = Intent(this, GameOverviewActivity::class.java)
         intent.putExtra(StaticTags.GAME_ID_TAG, game.id)
         startGameOverviewActivity.launch(intent)
-        Log.d("LoadGameActivity", "gay game" + game.id.toString())
+    }
+
+    private fun deleteGame(game: Game) {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.confirm_delete_title))
+            .setMessage(getString(R.string.confirm_delete_body))
+            .setPositiveButton(getString(R.string.yes)) { _, _ -> viewModel.deleteGame(game) }
+            .setNegativeButton(getString(R.string.no), null)
+            .show()
+    }
+
+    private fun setMode(mode: SelectionMode) {
+        when (mode) {
+            SelectionMode.LAUNCH -> toolbar.title = getString(R.string.app_name)
+            SelectionMode.DELETE -> toolbar.title = getString(R.string.toolbar_delete_mode)
+        }
+        selectionMode = mode
+    }
+
+    private enum class SelectionMode {
+        LAUNCH,
+        DELETE
     }
 }
--- app/src/main/java/com/example/quizbowlscoresheet/LoadGameViewModel.kt
+++ app/src/main/java/com/example/quizbowlscoresheet/LoadGameViewModel.kt
@@ -3,11 +3,18 @@ package com.example.quizbowlscoresheet
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.quizbowlscoresheet.database.models.Game
 import com.example.quizbowlscoresheet.database.repositories.agqbagame.GameAGQBARepository
+import kotlinx.coroutines.launch
 
 class LoadGameViewModel(private val gameAGQBARepository: GameAGQBARepository): ViewModel() {
 
     val gameList = gameAGQBARepository.getGameWithTeamListFlow().asLiveData()
+
+    fun deleteGame(game: Game) = viewModelScope.launch {
+        gameAGQBARepository.deleteGame(game)
+    }
 
     class LoadGameViewModelFactory(private val repository: GameAGQBARepository) : ViewModelProvider.Factory{
         override fun <T: ViewModel> create(modelClass: Class<T>): T{
